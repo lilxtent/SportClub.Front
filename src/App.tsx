1b8522bc78107c39687bf355caeec7c5c677a660
lilxtent <lilxtent@gmail.com>
2023-04-15 17:07:11 +0700

    rsuite added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Content, CustomProvider, Header, Nav, Sidebar, Sidenav} from "rsuite";
+import {ruRU} from "rsuite/locales";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <CustomProvider locale={ruRU} theme={"dark"}>
+            <Container>
+                <Sidebar
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}
+                >
+                    <Sidenav appearance="default">
+                        <Sidenav.Body>
+                            <Nav>
+                                <Nav.Item>First</Nav.Item>
+                                <Nav.Item>Settings</Nav.Item>
+                            </Nav>
+                        </Sidenav.Body>
+                    </Sidenav>
+                </Sidebar>
+
+                <Container>
+                    <Header>
+                        <h2>Page Title</h2>
+                    </Header>
+                    <Content>Content</Content>
+                </Container>
+            </Container>
+        </CustomProvider>
+    );
 }
 
 export default App;
